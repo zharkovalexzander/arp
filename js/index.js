65a6da7628c54a793eeb18dfe5ef1db56a450338
Oleksandr Zharkov <oleksandr.zharkov@globallogic.com>
2018-04-04 23:26:50 +0300

    123

---
+++ js/index.js
@@ -0,0 +1,31 @@
+function main() {
+        
+    var map = L.map('map').setView([32.7766642, -96.7969879], 16);
+
+    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+        maxZoom: 18,
+        attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
+            '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+            'Imagery © <a href="http://mapbox.com">Mapbox</a>',
+        id: 'mapbox.light'
+    }).addTo(map);
+
+
+    L.geoJSON(freeBus, {
+
+        filter: function (feature, layer) {
+            if (feature.properties) {
+                return feature.properties.underConstruction !== undefined ? !feature.properties.underConstruction : true;
+            }
+            return false;
+        }
+
+    }).addTo(map);
+    
+    map.on('click', function(e) {
+         console.log("[" + e.latlng.lng + ", " + e.latlng.lat + "]");
+    });
+
+}
+    
+};
---
+++ js/sample-geojson.js
@@ -0,0 +1,170 @@
+var freeBus = {
+    "type": "FeatureCollection",
+    "features": [
+        {
+            "type": "Feature",
+            "geometry": {
+                "type": "LineString",
+                "coordinates": [
+                    [-96.8069100379944,32.7758008662042],
+[-96.80714607238771,32.7765946891179],
+[-96.80733919143677,32.77722613410597],
+[-96.80759668350221,32.7780560264186],
+[-96.80778980255128,32.7787235428795],
+[-96.8080472946167,32.779499298961355],
+[-96.80716753005983,32.77969774710519],
+[-96.80626630783082,32.77991423548457],
+[-96.80607318878175,32.779174564680375],
+[-96.80424928665161,32.77951733972],
+[-96.80403470993043,32.778831788320204],
+[-96.80283308029175,32.77903023795282],
+[-96.80283308029175,32.77870550195991],
+[-96.80261850357057,32.778092108517505],
+[-96.80248975753784,32.77760499894799],
+[-96.80221080780031,32.7767570611142],
+[-96.80218935012819,32.77663077180936],
+[-96.80274724960329,32.77664881314959],
+[-96.80341243743898,32.776558606411804],
+[-96.80424928665161,32.776378192661895],
+[-96.80530071258545,32.77614365424019],
+[-96.80611610412599,32.77596323964901],
+[-96.80693149566652,32.775782824692065]
+                ]
+            },
+            "properties": {
+                "popupContent": "This is a free bus line that will take you across downtown.",
+                "underConstruction": false
+            },
+            "id": 1
+        }
+    ]
+};
+
+var lightRailStop = {
+    "type": "FeatureCollection",
+    "features": [
+        {
+            "type": "Feature",
+            "properties": {
+                "popupContent": "18th & California Light Rail Stop"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [-104.98999178409576, 39.74683938093904]
+            }
+        },{
+            "type": "Feature",
+            "properties": {
+                "popupContent": "20th & Welton Light Rail Stop"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [-104.98689115047453, 39.747924136466565]
+            }
+        }
+    ]
+};
+
+var bicycleRental = {
+    "type": "FeatureCollection",
+    "features": [
+        {
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -104.9998241,
+                    39.7471494
+                ]
+            },
+            "type": "Feature",
+            "properties": {
+                "popupContent": "This is a B-Cycle Station. Come pick up a bike and pay by the hour. What a deal!"
+            },
+            "id": 51
+        },
+        {
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -104.9983545,
+                    39.7502833
+                ]
+            },
+            "type": "Feature",
+            "properties": {
+                "popupContent": "This is a B-Cycle Station. Come pick up a bike and pay by the hour. What a deal!"
+            },
+            "id": 52
+        },
+        {
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -104.9963919,
+                    39.7444271
+                ]
+            },
+            "type": "Feature",
+            "properties": {
+                "popupContent": "This is a B-Cycle Station. Come pick up a bike and pay by the hour. What a deal!"
+            },
+            "id": 54
+        },
+        {
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -104.9960754,
+                    39.7498956
+                ]
+            },
+            "type": "Feature",
+            "properties": {
+                "popupContent": "This is a B-Cycle Station. Come pick up a bike and pay by the hour. What a deal!"
+            },
+            "id": 55
+        },
+        {
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -104.9933717,
+                    39.7477264
+                ]
+            },
+            "type": "Feature",
+            "properties": {
+                "popupContent": "This is a B-Cycle Station. Come pick up a bike and pay by the hour. What a deal!"
+            },
+            "id": 57
+        },
+        {
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -104.9913392,
+                    39.7432392
+                ]
+            },
+            "type": "Feature",
+            "properties": {
+                "popupContent": "This is a B-Cycle Station. Come pick up a bike and pay by the hour. What a deal!"
+            },
+            "id": 58
+        },
+        {
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -104.9788452,
+                    39.6933755
+                ]
+            },
+            "type": "Feature",
+            "properties": {
+                "popupContent": "This is a B-Cycle Station. Come pick up a bike and pay by the hour. What a deal!"
+            },
+            "id": 74
+        }
+    ]
+};
